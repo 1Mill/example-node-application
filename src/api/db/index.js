e73b342566a677b5607809d666788bf7d03c508e
1Mill <erik.ekberg@outlook.com>
2020-01-22 21:32:49 -0800

    Add port to env

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -2,6 +2,6 @@ module.exports = {
 	database: 'postgres',
 	host: 'database', // Docker database service name
 	password: process.env.DATABASE_PASSWORD,
-	port: '5432',
+	port: process.env.DATABASE_PORT,
 	user: process.env.DATABASE_USER,
 }
