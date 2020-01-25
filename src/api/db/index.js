7459ff3cfd8ee6761cb6fae476e30cc659ee6d2d
1Mill <erik.ekberg@outlook.com>
2020-01-25 14:34:02 -0800

    Connect api to database

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -1,7 +1,22 @@
-module.exports = {
+const { Pool, Client } = require('pg')
+
+const config = {
 	database: 'postgres',
 	host: 'database', // Docker database service name
 	password: process.env.DATABASE_PASSWORD,
 	port: process.env.DATABASE_PORT,
 	user: process.env.DATABASE_USER,
 }
+
+const pool = new Pool(config)
+pool.query('SELECT NOW()', (err, res) => {
+	console.log(err, res)
+	pool.end();
+})
+
+const client = new Client(config)
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+	console.log(err, res)
+	client.end()
+})
