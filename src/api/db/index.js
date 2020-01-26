5d754296a2d04fcc57e2bdfec1e2c1d1f8b06046
1Mill <erik.ekberg@outlook.com>
2020-01-26 14:53:37 -0800

    Scaffold DATABASE_HOST env

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 const config = {
 	database: 'postgres',
-	host: 'database', // Docker database service name
+	host: process.env.DATABASE_HOST, // Docker database service name
 	password: process.env.DATABASE_PASSWORD,
 	port: process.env.DATABASE_PORT,
 	user: process.env.DATABASE_USER,
