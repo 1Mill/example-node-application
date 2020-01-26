d590784958d6eeea6db4a1187e7eb5e316d5b93c
1Mill <erik.ekberg@outlook.com>
2020-01-26 15:51:35 -0800

    Scaffold way to migrate database

---
+++ src/api/knexfile.js
@@ -0,0 +1,21 @@
+const { config } = require('./db')
+
+const MIGRATION_DIRECTORY_PATH = '/migrations';
+const MIGRATION_TABLE_NAME = 'knex_migrations';
+
+const defaults = {
+	client: 'postgresql',
+	connection: config,
+	migrations: MIGRATION_TABLE_NAME,
+	pool: { min: 2, max: 10 },
+}
+
+module.exports = {
+	migrations: {
+		directory: [MIGRATION_DIRECTORY_PATH],
+	},
+
+	development: defaults,
+	production: defaults,
+	staging: defaults,
+}
