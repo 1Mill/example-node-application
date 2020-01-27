c257d6a848aa03da8be2fbc79784f78484cda831
1Mill <erik.ekberg@outlook.com>
2020-01-26 20:34:54 -0800

    Update 20200127002345_create-todos.js

--- src/api/db/migrations/20200127002345_create-todos.js
+++ src/api/db/migrations/20200127002345_create-todos.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+	return knex.schema.createTable('todos', (table) => {
+		table.increments()
+		table.timestamps()
+
+		table.boolean('completed')
+		table.text('body')
+	});
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema.dropTable('todos')
 };
