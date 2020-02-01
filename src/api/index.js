c002f2f17433b6d9a05d75dab1033c3fd2065ade
1Mill <erik.ekberg@outlook.com>
2020-02-01 13:34:20 -0800

    Catch errors

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ db.migrate()
 
 db.query('SELECT * FROM todos')
 	.then(res => console.log(res.rows))
+	.catch(err => console.error(err))
 
 // const statement = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
 // const values = ['myValue', 'myValue@html.erb']
