8cfa06a257b67223d69a23f53414de551185fee2
1Mill <erik.ekberg@outlook.com>
2020-02-01 13:38:44 -0800

    Wait for migrations to finish, then start executing main code

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
 const { db } = require('./db');
 
+const main = () => {
+	db.query('SELECT * FROM todos')
+		.then(res => console.log(res.rows))
+		.catch(err => console.error(err))
+
+	// const statement = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+	// const values = ['myValue', 'myValue@html.erb']
+	// db.query(statement, values);
+}
+
 db.migrate()
-
-db.query('SELECT * FROM todos')
-	.then(res => console.log(res.rows))
+	.then(() => main())
 	.catch(err => console.error(err))
-
-// const statement = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
-// const values = ['myValue', 'myValue@html.erb']
-// db.query(statement, values);
