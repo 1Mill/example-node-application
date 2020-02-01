7db871241dda99b8af859ba2d7998db0713de0ce
1Mill <erik.ekberg@outlook.com>
2020-02-01 13:00:31 -0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,10 @@ const { db } = require('./db');
 db.query('SELECT NOW()')
 	.then(res => console.log(res))
 	.catch(err => console.error(err))
-	.finally()
+	.finally(() => process.exit(0))
 
 console.log("I did a thing.")
 
 // const statement = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
 // const values = ['myValue', 'myValue@html.erb']
 // db.query(statement, values);
-
-process.exit(0);
