70ca2dc3e51ee0481b5f45bbd53dff298badc119
1Mill <erik.ekberg@outlook.com>
2020-02-01 13:11:13 -0800

    Fix path after moving file

--- src/api/knexfile.js
+++ src/api/knexfile.js
@@ -1,5 +1,5 @@
 const { MIGRATION_DIRECTORY_RELATIVE_PATH, MIGRATION_TABLE_NAME } = require('./db/lib/migration')
-const { config } = require('./db')
+const { config } = require('./db/config')
 
 const defaults = {
 	client: 'postgresql',
