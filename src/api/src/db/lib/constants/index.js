86d4ada7631be09f9297821ef2b510a6325d21f5
1Mill <erik.ekberg@outlook.com>
2020-02-02 13:04:03 -0800

    Update migration directory path

--- src/api/src/db/lib/constants/index.js
+++ src/api/src/db/lib/constants/index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	constants: {
-		MIGRATION_DIRECTORY_RELATIVE_PATH: './db/migrations',
+		MIGRATION_DIRECTORY_RELATIVE_PATH: './src/db/migrations',
 		MIGRATION_TABLE_NAME: 'knex_migrations',
 	}
 }
